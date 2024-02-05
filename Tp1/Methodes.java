cbe17b4b789a0b5a3124684d25302c1c3175e03e
Istark <154691996+Istaarkk@users.noreply.github.com>
2024-02-05 11:24:58 +0100

    Methodes.java

--- Tp1/Methodes.java
+++ Tp1/Methodes.java
@@ -2,17 +2,13 @@ public class Methodes {
     public static void main(String[] args) {
         char char1 = 'A';
         char char2 = 'B';
-
-        System.out.println("Avant l'appel : char1 = " + char1 + ", char2 = " + char2);
+        
         echangerChars(char1, char2);
-        System.out.println("Après l'appel : char1 = " + char1 + ", char2 = " + char2);
 
         float[] tableauFloat = new float[1];
         tableauFloat[0] = 100.0f;
 
-        System.out.println("Avant l'appel : tableauFloat[0] = " + tableauFloat[0]);
         modifierTableau(tableauFloat);
-        System.out.println("Après l'appel : tableauFloat[0] = " + tableauFloat[0]);
     }
 
     public static void echangerChars(char c1, char c2) {
@@ -20,7 +16,7 @@ public class Methodes {
         char temp = c1;
         c1 = c2;
         c2 = temp;
-        System.out.println("Pendant l'appel : c1 = " + c1 + ", c2 = " + c2);
+        System.out.println("Après l'appel : c1 = " + c1 + ", c2 = " + c2);
     }
 
     public static void modifierTableau(float[] tableau) {
